137059ca22309e11c4ed9602bd53eac9853d9794
David243 <Davidwong11219@gmail.com>
2022-09-19 10:35:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,19 +24,6 @@ let weather =
         }
     };
 
-
-//first function
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} where is {city name} first parameter and {API key} second parameter. q= inputs
-//Once API key was activated you can see all information need it. To change temperature units we have to look at API: units=metric  https://api.openweathermap.org/data/2.5/weather?q=NewYorkCity&units=metric&appid=d815cf48153ad79ce73196e79191001c
-//Now we can allow to work in any city: q=Brooklyn change to q=" + city + " (city is parameter) and "units=imperial&appid=" (it taken from url above and delete api key and subscituted with variable apiKey) + apiKey; "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey; To make them more readable start them in new line. appid = this.apiKey;
-//We can write new function that will display the weather on a page displayWeather: that will be the function that take data and display weather
-//here end of first function description
-
-//Exta
-//To change JSON lower case data to capital letter - select that class then text-transofm: Capitalize
-
-
-
 //Searches when user presses the search button
 document.querySelector(".search button").addEventListener('click', function(){
     weather.search();
